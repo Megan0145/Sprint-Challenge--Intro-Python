39e58e7095c78135ff3a50ea0893abfda7dad066
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-14 13:53:50 +0000

    Refactor to list comprehension

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -61,9 +61,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
-for human in humans:
-    g.append(Human(human.name.upper(), human.age + 5))
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
