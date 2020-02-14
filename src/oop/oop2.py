e5ebf98ab7637ae1b6ba91d7efca4893648497c1
Megan Ennis <Megan@Megans-iMac-9.local>
2020-02-14 09:01:07 +0000

    Add motorcyle class as subclass of ground vehicle. Default num wheels to 2 and override ground vehicle drive method

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -20,6 +20,12 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+    def drive(self):
+        return "BRAAAP!!"
+
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
